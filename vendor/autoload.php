c2b6ea1f94431f1751798e58b595351c03290d1a
Ken <kenvunz@gmail.com>
2013-02-01 11:45:13 +1300

    composer update run

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit66cd839c8c9b68ca91dbbe29b003ddc2::getLoader();
+return ComposerAutoloaderInitcedf01ea147b165983419dfc99c8a45f::getLoader();
