1b679a558698cfe13209164f1e7e251f5a77105e
Ken <kenvunz@gmail.com>
2013-02-14 10:17:27 +0700

    extend the blade compiler to handle the @loop tag

--- src/Fancy/Core/CoreServiceProvider.php
+++ src/Fancy/Core/CoreServiceProvider.php
@@ -109,6 +109,41 @@ class CoreServiceProvider extends ServiceProvider {
             // instance to pass into the engine so it can compile the views properly.
             $compiler = new BladeCompiler($app['files'], $cache);
 
+            $compiler->extend(function($value) {
+                $pattern = '/(?<!\w)(\s*)@(loop)(\s*\(.*\))/';
+
+                $matches = array();
+                preg_match($pattern, $value, $matches);
+
+                $arguments = preg_replace('/\s*/', '', $matches[3]);
+
+                $replacement = '$1<?php ';
+
+                if($arguments !== '()') {
+                    $replacement .= '$query = new WP_Query$3; if($query->have_posts()): while($query->have_posts()): $query->the_post()';
+                } else {
+                    $replacement .= 'if(have_posts()): while(have_posts()): the_post()';
+                }
+
+                $replacement .= '?>';
+
+                $value = preg_replace($pattern, $replacement, $value);
+
+                $pattern = '/(\s*)@(endloop)(\s*)/';
+
+                $replacement = '$1<?php endwhile; endif;';
+
+                if($arguments !== '()') {
+                    $replacement .= 'wp_reset_postdata();';
+                }
+
+                $replacement .= '?>$3';
+
+                $value = preg_replace($pattern, $replacement, $value);
+
+                return $value;
+            });
+
             return new CompilerEngine($compiler, $app['files']);
         });
 	}
