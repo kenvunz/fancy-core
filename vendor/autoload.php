bc568be904d7df89f8d508378d45154f4b00d369
Ken <kenvunz@gmail.com>
2013-02-25 16:17:49 +0700

    update illuminate/support module

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0e05b44fa36ed9b2009747b11246ad8e::getLoader();
+return ComposerAutoloaderInitbe135768db45f5ec96a46ec9a15f66fc::getLoader();
